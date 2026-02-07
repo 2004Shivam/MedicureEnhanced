136a7161b75cdd894deafb65d4861384c5fceedb
2004shivam <shivamyadav69998@gmail.com>
2026-02-07 12:57:41 +0530

    feat: add build.sh to auto-populate doctors on deploy

---
+++ medicure/build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# Build script for Render deployment
+
+set -o errexit
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Collect static files
+python manage.py collectstatic --no-input
+
+# Run migrations
+python manage.py migrate
+
+# Populate sample doctors (only creates if they don't exist)
+python manage.py populate_doctors
