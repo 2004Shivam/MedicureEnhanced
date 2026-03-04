f5af09569df0af90725d696a2de8c71a5d6b4bd9
2004shivam <shivamyadav69998@gmail.com>
2026-03-04 23:31:40 +0530

    Added Selenium screenshot capture

--- medicure-tests/src/test/java/MedicureTest.java
+++ medicure-tests/src/test/java/MedicureTest.java
@@ -2,28 +2,51 @@ import org.testng.annotations.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 
 public class MedicureTest {
 
-@Test
-public void openWebsite() {
+    @Test
+    public void openWebsite() throws IOException {
 
-WebDriverManager.chromedriver().setup();
+        System.out.println("Setting up Chrome driver...");
 
-ChromeOptions options = new ChromeOptions();
-options.addArguments("--headless=new");
-options.addArguments("--no-sandbox");
-options.addArguments("--disable-dev-shm-usage");
+        WebDriverManager.chromedriver().setup();
 
-WebDriver driver = new ChromeDriver(options);
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
 
-driver.get("https://medicureenhanced.onrender.com");
+        WebDriver driver = new ChromeDriver(options);
 
-System.out.println("Page Title: " + driver.getTitle());
+        System.out.println("Opening MediCure website...");
 
-driver.quit();
+        driver.get("https://medicure-enhanced.onrender.com");
 
-}
+        System.out.println("Page Title: " + driver.getTitle());
+        System.out.println("Current URL: " + driver.getCurrentUrl());
 
+        System.out.println("Taking screenshot...");
+
+        File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+
+        File destination = new File("medicure_screenshot.png");
+
+        Files.copy(screenshot.toPath(), destination.toPath(), StandardCopyOption.REPLACE_EXISTING);
+
+        System.out.println("Screenshot saved as medicure_screenshot.png");
+
+        driver.quit();
+
+        System.out.println("Test completed successfully.");
+    }
 }
